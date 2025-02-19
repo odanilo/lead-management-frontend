a7226d8a93901a519adc88851804e4b4e8e94e20
Danilo <dan.nunes0@gmail.com>
2025-02-19 13:42:49 -0300

    feat: add toast notifications

--- src/features/Timeline/hooks/usePatchLeadStatus.tsx
+++ src/features/Timeline/hooks/usePatchLeadStatus.tsx
@@ -3,14 +3,31 @@ import { patchLeadStatusService } from '../api/TimelineService';
 import { PatchLeadStatusRepositoyRequest } from '../api/TimelineApiTypes';
 import { queryClient } from '@/lib/queryClient';
 import { getGetLeadsQueryKey } from './useGetLeads';
+import { useToast } from '@/hooks/use-toast';
 
 export function usePatchLeadStatus() {
+  const { toast } = useToast();
+
   const mutation = useMutation({
     mutationFn: (request: PatchLeadStatusRepositoyRequest) =>
       patchLeadStatusService(request),
     onSuccess: async (_, request) => {
       await queryClient.invalidateQueries({
         queryKey: getGetLeadsQueryKey(request.newStatus),
+      });
+
+      toast({
+        title: 'Lead alterado com sucesso',
+        description: `Agora o lead estará com o status ${request.newStatus}`,
+      });
+    },
+    onError: (error) => {
+      const errorDescription = error.message || error;
+
+      toast({
+        title: 'Não foi possível alterar o lead',
+        description: `${errorDescription}`,
+        variant: 'destructive',
       });
     },
   });
