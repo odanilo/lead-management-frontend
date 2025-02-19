0982710399ec4df6d5e07b463c7d1f2e17e33a5e
Danilo <dan.nunes0@gmail.com>
2025-02-19 15:17:06 -0300

    feat: add loading states for buttons and page

--- src/features/Timeline/views/TimelineListView.tsx
+++ src/features/Timeline/views/TimelineListView.tsx
@@ -9,13 +9,34 @@ import { TimelineCardFooter } from '../components/TimelineCard/TimelineCardFoote
 import { TimelineCardActions } from '../components/TimelineCard/TimelineCardActions';
 import { Button } from '@/components/ui/button';
 import { LeadPrice } from '../components/TimelineCard/LeadPrice';
+import { LoadingSpinner } from '@/components/loading/LoadingSpiner';
 
 export function TimelineListView() {
-  const { data: getPendingLeadsData, refetch: getPendingLeadsRefetch } =
-    useGetLeads({ status: 'PENDING' });
-  const { data: getAcceptedLeadsData, refetch: getAcceptedLeadsRefetch } =
-    useGetLeads({ status: 'ACCEPTED' });
-  const { mutate: patchLeadStatusMutate } = usePatchLeadStatus();
+  const {
+    data: getPendingLeadsData,
+    refetch: getPendingLeadsRefetch,
+    isPending: isGetPeadingLeadsPending,
+  } = useGetLeads({ status: 'PENDING' });
+  const {
+    data: getAcceptedLeadsData,
+    refetch: getAcceptedLeadsRefetch,
+    isPending: isGetAcceptedLeadsPending,
+  } = useGetLeads({ status: 'ACCEPTED' });
+  const {
+    mutate: patchLeadStatusMutate,
+    isPending: isPatchLeadStatusPending,
+    variables: patchLeadStatusVariables,
+  } = usePatchLeadStatus();
+
+  const isPending = isGetPeadingLeadsPending || isGetAcceptedLeadsPending;
+
+  if (isPending) {
+    return (
+      <div className="mt-6 flex justify-center">
+        <LoadingSpinner />
+      </div>
+    );
+  }
 
   return (
     <Tabs defaultValue="invited">
@@ -28,53 +49,71 @@ export function TimelineListView() {
         </TabsTrigger>
       </TabsList>
 
-      <TabsContent value="invited" className="mt-6 flex flex-col gap-4">
-        {getPendingLeadsData?.map((lead) => (
-          <TimelineCard key={lead.id} lead={lead}>
-            <TimelineCardMain>
-              <LeadDescription>{lead.description}</LeadDescription>
-            </TimelineCardMain>
+      <section className="mt-6 [&>_*]:flex [&>_*]:flex-col [&>_*]:gap-4">
+        <TabsContent value="invited">
+          {getPendingLeadsData?.map((lead) => {
+            const isPending =
+              patchLeadStatusVariables?.leadId === lead.id &&
+              isPatchLeadStatusPending;
 
-            <TimelineCardFooter>
-              <TimelineCardActions className="flex items-center gap-2">
-                <Button
-                  onClick={() =>
-                    patchLeadStatusMutate({
-                      leadId: lead.id,
-                      newStatus: 'ACCEPTED',
-                    })
-                  }
-                >
-                  Accept
-                </Button>
-                <Button
-                  variant="secondary"
-                  onClick={() =>
-                    patchLeadStatusMutate({
-                      leadId: lead.id,
-                      newStatus: 'DECLINED',
-                    })
-                  }
-                >
-                  Decline
-                </Button>
-              </TimelineCardActions>
+            return (
+              <TimelineCard key={lead.id} lead={lead}>
+                <TimelineCardMain>
+                  <LeadDescription>{lead.description}</LeadDescription>
+                </TimelineCardMain>
 
-              <LeadPrice>{lead.price}</LeadPrice>
-            </TimelineCardFooter>
-          </TimelineCard>
-        ))}
-      </TabsContent>
-      <TabsContent value="accepted" className="mt-6 flex flex-col gap-4">
-        {getAcceptedLeadsData?.map((lead) => (
-          <TimelineCard key={lead.id} lead={lead}>
-            <TimelineCardMain>
-              <LeadContactInformation lead={lead} />
-              <LeadDescription>{lead.description}</LeadDescription>
-            </TimelineCardMain>
-          </TimelineCard>
-        ))}
-      </TabsContent>
+                <TimelineCardFooter>
+                  <TimelineCardActions className="flex items-center gap-2">
+                    <Button
+                      onClick={() =>
+                        patchLeadStatusMutate({
+                          leadId: lead.id,
+                          newStatus: 'ACCEPTED',
+                        })
+                      }
+                      disabled={isPending}
+                    >
+                      Accept
+                      {isPending &&
+                      patchLeadStatusVariables.newStatus === 'ACCEPTED' ? (
+                        <LoadingSpinner />
+                      ) : null}
+                    </Button>
+                    <Button
+                      variant="secondary"
+                      onClick={() =>
+                        patchLeadStatusMutate({
+                          leadId: lead.id,
+                          newStatus: 'DECLINED',
+                        })
+                      }
+                      disabled={isPending}
+                    >
+                      Decline
+                      {isPending &&
+                      patchLeadStatusVariables.newStatus === 'DECLINED' ? (
+                        <LoadingSpinner />
+                      ) : null}
+                    </Button>
+                  </TimelineCardActions>
+
+                  <LeadPrice>{lead.price}</LeadPrice>
+                </TimelineCardFooter>
+              </TimelineCard>
+            );
+          })}
+        </TabsContent>
+        <TabsContent value="accepted">
+          {getAcceptedLeadsData?.map((lead) => (
+            <TimelineCard key={lead.id} lead={lead}>
+              <TimelineCardMain>
+                <LeadContactInformation lead={lead} />
+                <LeadDescription>{lead.description}</LeadDescription>
+              </TimelineCardMain>
+            </TimelineCard>
+          ))}
+        </TabsContent>
+      </section>
     </Tabs>
   );
 }
