19631b1e379196b8d22826cebee01099d6fd7254
Danilo <dan.nunes0@gmail.com>
2025-02-19 09:18:27 -0300

    chore: add tanstack for data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+
 import { Container } from '@/components/container/Container';
 import { TimelineListView } from './features/Timeline/views/TimelineListView';
+import { queryClient } from './lib/queryClient';
 
 export function App() {
   return (
-    <Container className="py-8">
-      <TimelineListView />
-    </Container>
+    <QueryClientProvider client={queryClient}>
+      <Container className="py-8">
+        <TimelineListView />
+      </Container>
+    </QueryClientProvider>
   );
 }
