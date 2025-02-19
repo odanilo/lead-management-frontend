aa713d450202386f8ac5d9bf105d6376d7b2d906
Danilo <dan.nunes0@gmail.com>
2025-02-19 16:57:48 -0300

    refactor: simulating a inital loading

--- src/features/Timeline/api/TimelineRepository.ts
+++ src/features/Timeline/api/TimelineRepository.ts
@@ -1,23 +1,35 @@
 import { httpClient } from '@/lib/httpClient';
 import {
   GetLeadsRepositoryParams,
+  GetLeadsRepositoryResponse,
   Lead,
   PatchLeadStatusRepositoyRequest,
 } from './TimelineApiTypes';
 import { mockLeads } from '../utils/Timeline.utils';
 
-export const getLeadsRepository = async (params?: GetLeadsRepositoryParams) => {
+export const getLeadsRepository = async (
+  params?: GetLeadsRepositoryParams,
+): Promise<{ data: GetLeadsRepositoryResponse }> => {
   // return await httpClient.get<GetLeadsRepositoryResponse>('leads', {
   //   params,
   // });
 
-  if (params && (params.status === 'ACCEPTED' || params.status === 'PENDING')) {
-    return { data: mockLeads.filter((lead) => lead.status === params.status) };
-  }
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      if (
+        params &&
+        (params.status === 'ACCEPTED' || params.status === 'PENDING')
+      ) {
+        return resolve({
+          data: mockLeads.filter((lead) => lead.status === params.status),
+        });
+      }
 
-  return {
-    data: mockLeads,
-  };
+      return resolve({
+        data: mockLeads,
+      });
+    }, 1200);
+  });
 };
 
 export const patchLeadStatusRepository = async ({
